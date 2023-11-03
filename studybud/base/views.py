18eea5b7d7ffbd90aae64d96d7245cfe7dc0c3f7
laffer-ryan <lafferty.ryan10@gmail.com>
2023-11-03 17:59:21 +1300

    Added search capability

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render, redirect
+from django.db.models import Q # Allows us to add in and | or statements to queries
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
@@ -14,14 +15,22 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all() 
 
-    topic = Topic.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
+    topics = Topic.objects.all()
+    room_count = rooms.count()
 
 
 
     context = {'rooms': rooms,
-                'topics': topic}
+                'topics': topics,
+                'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
