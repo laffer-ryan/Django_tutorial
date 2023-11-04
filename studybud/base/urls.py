30606358f5a0a8de26fba8d6d9f06d5479f679c3
laffer-ryan <lafferty.ryan10@gmail.com>
2023-11-04 19:12:42 +1300

    Added recent activity component with functionality

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout.html', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name="room"),
